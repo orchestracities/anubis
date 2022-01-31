e7ac6ae552cab36b0ef2e7b5e7bee909cd193074
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2022-01-31 14:49:28 +0100

    API Test setup

--- auth-management-api/src/database.py
+++ auth-management-api/src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+import os, pytest
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://api:password@postgresserver/policyapi"
@@ -15,3 +15,9 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
---
+++ auth-management-api/src/test_main.py
@@ -0,0 +1,42 @@
+from fastapi.testclient import TestClient
+
+from main import app
+from database import Base
+from database import test_db as test_db
+
+client = TestClient(app)
+
+def test_tenants(test_db):
+    response = client.post(
+        "/v1/tenants/",
+        json={"name": "test"}
+    )
+    assert response.status_code == 201
+    tenant_id = response.headers["tenant-id"]
+    assert tenant_id
+
+    response = client.get("/v1/tenants/")
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 1
+
+    response = client.get("/v1/tenants/"+tenant_id)
+    body = response.json()
+    assert response.status_code == 200
+    assert body["name"] == "test"
+
+    response = client.get("/v1/tenants/"+tenant_id+"/service_paths/")
+    body = response.json()
+    assert response.status_code == 200
+    assert body[0]["path"] == "/"
+
+    response = client.post(
+        "/v1/tenants/"+tenant_id+"/service_paths",
+        json={"path": "/foobar"}
+    )
+    assert response.status_code == 201
+
+    response = client.get("/v1/tenants/"+tenant_id+"/service_paths/")
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 2
