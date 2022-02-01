4ec69ae237001512ce11f25dabe5893a8cd491a9
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2022-02-01 16:38:18 +0100

    Reorganized Tests

--- auth-management-api/src/database.py
+++ auth-management-api/src/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-import pytest
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://api:password@postgresserver/policyapi"
@@ -16,10 +15,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-@pytest.fixture()
-def test_db():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
--- auth-management-api/src/tests/test_tenants.py
+++ auth-management-api/src/tests/test_tenants.py
@@ -1,11 +1,9 @@
 from fastapi.testclient import TestClient
 
 from main import app
-from database import Base
-from database import test_db as test_db
+from .utils import test_db
 
 client = TestClient(app)
-
 
 def test_tenants(test_db):
     response = client.post(
@@ -21,23 +19,33 @@ def test_tenants(test_db):
     assert response.status_code == 200
     assert len(body) == 1
 
-    response = client.get("/v1/tenants/" + tenant_id)
+    response = client.get("/v1/tenants/"+tenant_id)
     body = response.json()
     assert response.status_code == 200
     assert body["name"] == "test"
 
-    response = client.get("/v1/tenants/" + tenant_id + "/service_paths/")
+
+def test_service_paths(test_db):
+    response = client.post(
+        "/v1/tenants/",
+        json={"name": "test"}
+    )
+    assert response.status_code == 201
+    tenant_id = response.headers["tenant-id"]
+    assert tenant_id
+
+    response = client.get("/v1/tenants/"+tenant_id+"/service_paths/")
     body = response.json()
     assert response.status_code == 200
     assert body[0]["path"] == "/"
 
     response = client.post(
-        "/v1/tenants/" + tenant_id + "/service_paths",
+        "/v1/tenants/"+tenant_id+"/service_paths",
         json={"path": "/foobar"}
     )
     assert response.status_code == 201
 
-    response = client.get("/v1/tenants/" + tenant_id + "/service_paths/")
+    response = client.get("/v1/tenants/"+tenant_id+"/service_paths/")
     body = response.json()
     assert response.status_code == 200
     assert len(body) == 2
