36cb19a50ab05c070c99f9a88af556812f299cdb
Federico M. Facca <chicco785@users.noreply.github.com>
2022-03-19 11:50:13 +0100

    fix cors testing

--- scripts/test_cors.sh
+++ scripts/test_cors.sh
@@ -1,13 +1,27 @@
 #!/bin/bash -e
 
-echo "Does the API support CORS?"
+echo "Does the API support CORS for localhost?"
 echo "==============================================================="
-export response=`curl -i -s -H "Origin: http://localhost" http://127.0.0.1:8000/v1/tenants/`
+export response=`curl -i -s -H "Origin: http://localhost" -H "Access-Control-Request-Method: GET" http://127.0.0.1:8085/v1/tenants/`
 if [[ "$response" == *"access-control-allow-origin"* ]]
 then
   echo "access-control-allow-origin header is present"
 else
   echo "ERROR: CORS not setup correctly"
+  exit 1
+fi
+
+echo ""
+
+echo "Does the API support CORS for example.com?"
+echo "==============================================================="
+export response=`curl -i -s -H "Origin: http://example.com" -H "Access-Control-Request-Method: GET" http://127.0.0.1:8085/v1/tenants/`
+if [[ "$response" == *"access-control-allow-origin"* ]]
+then
+  echo "ERROR: access-control-allow-origin header is present"
+else
+  echo "CORS setup correctly. http://example.com is not allowed as origin"
+  exit 1
 fi
 
 echo ""
