2f33ca55462418b42c1b5b7f6cf8b2eabc9a41f4
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2022-04-21 15:03:38 +0200

    Test fix

--- anubis-management-api/src/tests/utils.py
+++ anubis-management-api/src/tests/utils.py
@@ -1,10 +1,14 @@
 from database import Base
 from database import engine
+from tenants import models as t_models
+from policies import models as p_models
 import pytest
 
 
 @pytest.fixture()
 def test_db():
     Base.metadata.create_all(bind=engine)
+    p_models.insert_initial_agent_type_values()
+    p_models.insert_initial_mode_values()
     yield
     Base.metadata.drop_all(bind=engine)
