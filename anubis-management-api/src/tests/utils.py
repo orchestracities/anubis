07ec4439f7c0accd5e1e53e182973cfffa3ce11f
Federico M. Facca <chicco785@users.noreply.github.com>
2022-04-26 18:31:04 +0200

    update tests

--- anubis-management-api/src/tests/utils.py
+++ anubis-management-api/src/tests/utils.py
@@ -1,5 +1,5 @@
 from database import Base
-from database import engine
+from database import autocommit_engine
 from tenants import models as t_models
 from policies import models as p_models
 import pytest
@@ -7,8 +7,6 @@ import pytest
 
 @pytest.fixture()
 def test_db():
-    Base.metadata.create_all(bind=engine)
-    p_models.insert_initial_agent_type_values()
-    p_models.insert_initial_mode_values()
+    Base.metadata.create_all(bind=autocommit_engine)
     yield
-    Base.metadata.drop_all(bind=engine)
+    Base.metadata.drop_all(bind=autocommit_engine)
