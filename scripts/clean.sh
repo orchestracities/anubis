e0604dfc12c39a82b8cac0aee1a599d034932a40
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2022-09-26 15:56:43 +0200

    Fixed up setup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,7 @@
 docker-compose down -v --remove-orphans
 docker-compose rm -fv
 docker image rm orchestracities/anubis-management-api:test
+docker image rm orchestracities/middleware:test
 docker image rm orchestracities/configuration-api:latest
 docker image rm orchestracities/management-ui:latest
 
