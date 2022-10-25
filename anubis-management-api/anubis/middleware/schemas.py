b6247067e5252f7b020f36d6e1f19f9396f69aaf
Federico M. Facca <chicco785@users.noreply.github.com>
2022-10-25 11:34:48 +0200

    create endpoint specific for middleware interaction (it does not require service and servicePath)

--- anubis-management-api/anubis/middleware/schemas.py
+++ anubis-management-api/anubis/middleware/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 
+# TODO it would be good to have also the list of owners, but query needs to be defined
 class ResourceBase(BaseModel):
     id: str
     type: str
